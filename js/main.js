ed030826c9a58e9fe88296640ba1c7de430d56a7
Courtney Noonan <courtneynoonan@me.com>
2016-07-08 14:23:19 -0400

    finishing switch assignment

--- js/main.js
+++ js/main.js
@@ -1,2 +1,19 @@
 
+$(document).on('ready', function() {
 
+  $(".switch button").click(
+    function(event) {
+      $(".switch button").toggleClass("off on");
+      $("body").toggleClass("light dark");
+
+      if ($('button').hasClass("off")){
+        $("h1").text("Hey! Who turned out the lights!");
+        console.log("its Dark");
+      } else {
+        $("h1").text("It's so bright in here!");
+        console.log("its light");
+      }
+    }
+  );
+
+});
